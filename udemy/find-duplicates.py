28381f35802e9e23a8117fc67f69eb6a97a9beb2
Matthew <matthew.olker@gmail.com>
2021-05-25 13:34:11 -0400

    5/25 and updated to 3.9.5

---
+++ udemy/find-duplicates.py
@@ -0,0 +1,9 @@
+# Exercise Find Duplicates
+# count() returns how many of a value appears in a list
+some_list = ['a', 'b', 'c', 'b', 'd', 'm', 'n', 'n']
+
+duplicates = []
+for item in some_list:
+    if some_list.count(item) > 1 and item not in duplicates:
+        duplicates.append(item)
+print(duplicates)
