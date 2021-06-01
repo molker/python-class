706929afbace9ff58712dbdb618bb76ff893286f
Matthew <matthew.olker@gmail.com>
2021-06-01 11:27:31 -0400

    clean up

--- udemy/scripting/converter.py
+++ udemy/scripting/converter.py
@@ -3,12 +3,25 @@ import sys
 import os
 from PIL import Image
 
+'''
+just in case the user doesn't input with / at the end of
+the directory name
+'''
+
+
+def checkDirFormat(directory):
+    if directory.endswith('/'):
+        return directory
+    else:
+        return directory + '/'
+
+
 # grab first and second argument
 try:
-    source_dir = sys.argv[1]
-    target_dir = sys.argv[2]
+    source_dir = checkDirFormat(sys.argv[1])
+    target_dir = checkDirFormat(sys.argv[2])
 except IndexError:
-    print('No directories given, please provide arguments')
+    print('No arguments given, please provide source/target directories')
     sys.exit()
 
 # check is new/exists, if not create
@@ -20,10 +33,8 @@ if not os.path.isdir(target_dir):
     print(f'Directory {target_dir} created.')
 
 # loop through pokedex
-files = os.listdir(source_dir)
-
 # convert images to png
 # save to the new folder
-for img in files:
-    pokemon = Image.open(f'{source_dir}/{img}')
-    pokemon.save(target_dir + '/' + img.split('.')[0] + '.png', 'png')
+for img in os.listdir(source_dir):
+    pokemon = Image.open(f'{source_dir}{img}')
+    pokemon.save(target_dir + os.path.splitext(img)[0] + '.png', 'png')
