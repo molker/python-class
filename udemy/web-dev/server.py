9bf51c82167a53c8629be6de0926d39eff68af6c
Matthew <matthew.olker@gmail.com>
2021-06-21 13:26:46 -0400

    finished section 19

--- udemy/web-dev/server.py
+++ udemy/web-dev/server.py
@@ -1,5 +1,6 @@
 from flask import Flask, render_template
 from flask import Flask, render_template, url_for, request, redirect
+import csv
 app = Flask(__name__)
 
 
@@ -22,11 +23,24 @@ def write_to_file(data):
             f'\nEmail: {email},\nSubject: {subject},\nMessage: {message}\n')
 
 
+def write_to_csv(data):
+    with open('database.csv', newline='', mode='a') as database2:
+        email = data["email"]
+        subject = data["subject"]
+        message = data["message"]
+        csv_writer = csv.writer(database2, delimeter=',',
+                                quotechar='"', quoting=csv.QUOTE_MINIMAL)
+        csv_writer.writerow([email, subject, message])
+
+
 @app.route('/submit_form', methods=['POST', 'GET'])
 def submit_form():
     if request.method == 'POST':
-        data = request.form.to_dict()
-        write_to_file(data)
-        return redirect('./thankyou.html')
+        try:
+            data = request.form.to_dict()
+            write_to_file(data)
+            return redirect('./thankyou.html')
+        except:
+            return 'did not save to database'
     else:
         return 'something went wrong. try again'
